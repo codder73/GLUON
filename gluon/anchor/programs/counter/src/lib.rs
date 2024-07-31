77bc20641ab9341ff65ec29ce320102c09fa6605
Coder73 <codder73@gmail.com>
2024-07-31 02:08:48 +0000

    morning commit!

--- gluon/anchor/programs/counter/src/lib.rs
+++ gluon/anchor/programs/counter/src/lib.rs
@@ -8,63 +8,77 @@ declare_id!("6FLQ7wkjzdLcaauVwUX84swZpdeLPtRVWCsu4YtjahDM");
 pub mod counter {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseCounter>) -> Result<()> {
+  pub fn create_entry(
+    ctx: Context<CreateEntry>,
+    title: String,
+    message: String,
+  ) -> Result<()> {
+    msg!("Journal Entry Created");
+    msg!("Title: {}", title);
+    msg!("Message: {}", message);
+
+    let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.owner = ctx.accounts.owner.key();
+        journal_entry.title = title;
+        journal_entry.message = message;
+        Ok(())
+  }
+  pub fn update_journal_entry(
+    ctx: Context<UpdateEntry>,
+    title: String,
+    message: String,
+) -> Result<()> {
+    msg!("Journal Entry Updated");
+    msg!("Title: {}", title);
+    msg!("Message: {}", message);
+
+    let journal_entry = &mut ctx.accounts.journal_entry;
+    journal_entry.message = message;
+
     Ok(())
   }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.counter.count = ctx.accounts.counter.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.counter.count = ctx.accounts.counter.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeCounter>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.counter.count = value.clone();
-    Ok(())
-  }
-}
-
-#[derive(Accounts)]
-pub struct InitializeCounter<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Counter::INIT_SPACE,
-  payer = payer
-  )]
-  pub counter: Account<'info, Counter>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseCounter<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub counter: Account<'info, Counter>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub counter: Account<'info, Counter>,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Counter {
-  count: u8,
+pub struct JournalEntryState {
+    pub owner: Pubkey,
+    #[max_len(20)]
+    pub title: String,
+     #[max_len(200)]
+    pub message: String,
+    pub entry_id: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String, message: String)]
+pub struct CreateEntry<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        payer = owner,
+        space = 8 + JournalEntryState::INIT_SPACE
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String, message: String)]
+pub struct UpdateEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        realloc = 8 + 32 + 1 + 4 + title.len() + 4 + message.len(),
+        realloc::payer = owner,
+        realloc::zero = true,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
