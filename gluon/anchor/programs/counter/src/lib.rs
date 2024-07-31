7442ce0f8b980f17bf797225a99a79d68ddc4145
Coder73 <codder73@gmail.com>
2024-07-31 14:17:33 +0000

    completed anchor

--- gluon/anchor/programs/counter/src/lib.rs
+++ gluon/anchor/programs/counter/src/lib.rs
@@ -23,20 +23,30 @@ pub mod counter {
         journal_entry.message = message;
         Ok(())
   }
+
   pub fn update_journal_entry(
     ctx: Context<UpdateEntry>,
-    title: String,
-    message: String,
+    _title: String,
+    new_message: String,
 ) -> Result<()> {
     msg!("Journal Entry Updated");
-    msg!("Title: {}", title);
-    msg!("Message: {}", message);
+    msg!("Title: {}", _title);
+    msg!("Message: {}", new_message);
 
     let journal_entry = &mut ctx.accounts.journal_entry;
-    journal_entry.message = message;
+    journal_entry.message = new_message;
 
     Ok(())
   }
+
+  pub fn delete_journal_entry(
+    _ctx: Context<DeleteEntry>, 
+    title: String
+  ) -> Result<()> {
+    msg!("Journal entry titled {} deleted", title);
+    Ok(())
+}
+
 }
 
 #[account]
@@ -82,3 +92,19 @@ pub struct UpdateEntry<'info> {
     pub owner: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteEntry<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), owner.key().as_ref()],
+        bump,
+        close = owner,
+    )]
+    pub journal_entry: Account<'info, JournalEntryState>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
